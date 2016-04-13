b43b853f165991fd5969ac984352df217ac006ee
Ben Ronda <bronda95@comcast.net>
2016-04-12 20:39:41 -0700

    add glyphicon styling and else statement to helper for low answerd questions

--- app/helpers/post-popularity.js
+++ app/helpers/post-popularity.js
@@ -4,6 +4,8 @@ export function postPopularity(params) {
   var question = params[0];
 
   if(question.get('answers').get('length') >= 5){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span>');
+  } else {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
   }
 }
