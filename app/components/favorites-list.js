96829c745d8fecfb5689acacfd03b72ba49fe344
Ben Ronda <bronda95@comcast.net>
2016-04-14 11:08:08 -0700

    start adding computed property

--- app/components/favorites-list.js
+++ app/components/favorites-list.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service('favorites'),
+
+  titleAuthor: Ember.computed('favoritesList.question', 'favoritesList.author', function(){
+    return this.get('favoritesList.question') + ' : ' + this.get('favoritesList.author');
+  })
 });
